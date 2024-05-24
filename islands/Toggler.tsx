8bf4de75e717a90194b6fddc4e9bb80cdb23101e
Andy <the.andy.franklin@gmail.com>
2024-05-24 09:27:29 -0700

    initial commit

---
+++ islands/Toggler.tsx
@@ -0,0 +1,46 @@
+import { useSignal } from "@preact/signals";
+import axios from "axios";
+
+export default function Toggler() {
+  const showForm = useSignal(false);
+
+  const sendNewScore = async (e: Event) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const username = form.username.value;
+    const score = form.score.value;
+
+    await axios.post("http://localhost:3000/new-high-score", {
+      username,
+      score,
+    });
+
+    window.location.reload();
+  };
+
+  if (showForm.value) {
+    return (
+      <div className="bg-white rounded-lg">
+        <form onSubmit={sendNewScore}>
+          <div className="flex flex-col gap-2 m-12">
+            <input type="text" name="username" placeholder="username" className="border-2 border-black text-black" />
+            <input type="number" name="score" placeholder="score" className="border-2 border-black text-black" />
+            <div className="flex justify-end gap-2">
+              <button onClick={() => showForm.value = false} className="bg-gray-500 w-min p-2 rounded">Cancel</button>
+              <button type="submit" className="bg-blue-500 w-min p-2 rounded">Submit</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+
+  return (
+    <button
+      className="bg-blue-500 rounded p-2"
+      onClick={() => showForm.value = true}
+    >
+      Enter A New High Score
+    </button>
+  );
+}
---
+++ routes/index.tsx
@@ -0,0 +1,48 @@
+import { useSignal } from "@preact/signals";
+import Counter from "../islands/Counter.tsx";
+import { Handlers } from "$fresh/server.ts";
+import axios from "axios";
+import { z } from "zod";
+import { Failure, Success, Try } from "../utils/functions/try.ts";
+import Toggler from "../islands/Toggler.tsx";
+
+const schema = z.object({
+  scores: z.array(
+    z.object({
+      username: z.string(),
+      score: z.number(),
+    }),
+  ),
+}).transform((data) => data.scores);
+
+type Scores = z.infer<typeof schema>;
+
+export const handler: Handlers = {
+  async GET(req, ctx) {
+    const result = await Try(() =>
+      axios.get<unknown>("http://localhost:3000/all-scores")
+        .then(({ data }) => schema.parse(data))
+    );
+
+    return await ctx.render(result);
+  },
+};
+
+export default function Home({ data: result }: { data: Failure | Success<Scores> }) {
+  if (result.failure) return <div>{result.error.message}</div>;
+  const data = result.data;
+
+  const topThree = data.slice(0, 3);
+  const rest = data.slice(3);
+
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center gap-8 items-center bg-gray-800 text-white">
+      <div className="flex flex-col items-center">
+        {topThree.map(({ username, score }) => <p className="text-3xl">{username}: {score}</p>)}
+        <div className="mt-4" />
+        {rest.map(({ username, score }) => <p>{username}: {score}</p>)}
+      </div>
+      <Toggler />
+    </div>
+  );
+}
